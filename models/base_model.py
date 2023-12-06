32b7b9c452d40e59c84610bbfa154d7752ca6564
Amirametwally <amira.metwally56@gmail.com>
2023-12-06 20:57:35 +0200

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class BaseModel:
     """public instance attributes"""
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = (uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
