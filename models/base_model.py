2d73f221c1088b7fbb495e70443744e08ac398e9
Mos3aB696 <mos3ab696@gmail.com>
2023-12-07 23:23:00 +0200

    last updated

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
@@ -20,6 +21,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """The Str Function"""
@@ -30,6 +32,7 @@ class BaseModel:
         """update public instance attribute updated_at
         with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns dictionary containing all keys/values of the instance"""
