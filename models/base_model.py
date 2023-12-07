89274f8fc4ed3956d1e9ad69c5322605e3244605
Mos3aB696 <mos3ab696@gmail.com>
2023-12-07 17:56:16 +0200

    BaseModel file

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,27 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """public instance attributes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instancd of BaseModel"""
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        val, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """The Str Function"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """update public instance attribute updated_at
