03823d835ad71c63483ac12408a01418595fc20b
Amirametwally <amira.metwally56@gmail.com>
2023-12-09 22:16:45 +0200

    init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
-from .engine.file_storage import FileStorage
-"""The Init File"""
+#!/usr/bin/python3
 
+"""module: __init__"""
+from models.engine import file_storage
 
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
