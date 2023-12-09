1c8e160e049ec6e3d0aedea04c2002b33ac7a1a6
Amirametwally <amira.metwally56@gmail.com>
2023-12-10 00:31:50 +0200

    some editing

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,6 +7,7 @@ Unittestclasses:
 import os
 import models
 import unittest
+from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
@@ -41,6 +42,7 @@ class TestFileStorage_methods(unittest.TestCase):
     """Unittests for testing methods of the FileStorage class."""
 
     def setUp(self):
+        """setup for the tests"""
         pass
 
     def tearDown(self) -> None:
