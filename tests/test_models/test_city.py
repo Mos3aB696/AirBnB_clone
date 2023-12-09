e5c8fa21fdf166c9d6c19d25d2280892f9770624
Amirametwally <amira.metwally56@gmail.com>
2023-12-09 23:33:51 +0200

    done

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
 """Unit Tests For The `city` Module"""
+import unittest
+from models.city import City
+
+
+class Testcity(unittest.TestCase):
+    """"""
+
+    def setUp(self):
+        """"""
+        self.city = City()
+
+    def test_instance(self):
+        """"""
+        self.assertIsInstance(self.city, City)
+
+    def test_attributes(self):
+        """"""
+        self.assertTrue(hasattr(self.city, "state_id"))
+        self.assertEqual(self.city.state_id, "")
+
+        self.assertTrue(hasattr(self.city, "name"))
+        self.assertEqual(self.city.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
