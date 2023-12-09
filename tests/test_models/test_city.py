d5e3eb41df2166c019ef8b982887455539a76d5e
Amirametwally <amira.metwally56@gmail.com>
2023-12-09 23:35:43 +0200

    done

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,18 +5,18 @@ from models.city import City
 
 
 class Testcity(unittest.TestCase):
-    """"""
+    """TestCity class to test City class"""
 
     def setUp(self):
-        """"""
+        """setup for the tests"""
         self.city = City()
 
     def test_instance(self):
-        """"""
+        """test for instanatio"""
         self.assertIsInstance(self.city, City)
 
     def test_attributes(self):
-        """"""
+        """test attribute exist"""
         self.assertTrue(hasattr(self.city, "state_id"))
         self.assertEqual(self.city.state_id, "")
 
